0af2ce4c2497f6e06c79b702c71ccb8970b6d549
nicodeheza <nicolasdeheza@hotmail.com>
2022-03-04 17:25:39 -0300

    client nav bar mobile

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {UseUserName} from "../context/UserContext";
+import MenuBar from "../svg/MenuBar";
 import SmIcon from "../svg/SmIcon";
 import "./navBar.css";
 
 export default function NavBar() {
 	const {userName, setUserName} = UseUserName();
+	const [navOpen, setNavOpen] = useState(false);
 	useEffect(() => {
 		console.log(userName);
 	}, [userName]);
@@ -19,31 +21,55 @@ export default function NavBar() {
 		console.log("login");
 	}
 	return (
-		<nav className="navBar">
-			<div className="navBarIcon">
-				<SmIcon />
+		<nav className="navBarContainer">
+			<div className="navBar">
+				<div className="navBarIcon">
+					<SmIcon />
+				</div>
+				<div className="navBarLinks">
+					<div className="navBarStock">
+						<Link to="search">Stocks</Link>
+					</div>
+					<div className="navBarUser">
+						{userName ? (
+							<>
+								<Link to="portfolio">My Portfolio</Link>
+								<Link to="/" onClick={() => logOut()}>
+									Log Out
+								</Link>
+							</>
+						) : (
+							<>
+								<Link to="login" onClick={() => logIn()}>
+									Log In
+								</Link>
+								<Link to="singup">Sing Up</Link>
+							</>
+						)}
+					</div>
+				</div>
+				<div className="navBar-menuIcon" onClick={() => setNavOpen((pre) => !pre)}>
+					<MenuBar />
+				</div>
 			</div>
-			<div className="navBarLinks">
-				<div className="navBarStock">
-					<Link to="search">Stocks</Link>
-				</div>
-				<div className="navBarUser">
-					{userName ? (
-						<>
-							<Link to="portfolio">My Portfolio</Link>
-							<Link to="/" onClick={() => logOut()}>
-								Log Out
-							</Link>
-						</>
-					) : (
-						<>
-							<Link to="login" onClick={() => logIn()}>
-								Log In
-							</Link>
-							<Link to="singup">Sing Up</Link>
-						</>
-					)}
-				</div>
+			<div className={`navBar-mobile ${navOpen ? "nav-on" : "nav-off"}`}>
+				{userName ? (
+					<>
+						<Link to="search">Stocks</Link>
+						<Link to="portfolio">My Portfolio</Link>
+						<Link to="/" onClick={() => logOut()}>
+							Log Out
+						</Link>
+					</>
+				) : (
+					<>
+						<Link to="search">Stocks</Link>
+						<Link to="login" onClick={() => logIn()}>
+							Log In
+						</Link>
+						<Link to="singup">Sing Up</Link>
+					</>
+				)}
 			</div>
 		</nav>
 	);
---
+++ client/src/svg/MenuBar.tsx
@@ -0,0 +1,30 @@
+export default function MenuBar() {
+	return (
+		<svg
+			width="34"
+			height="27"
+			viewBox="0 0 34 27"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M0 2.5C0 1.11929 1.11929 0 2.5 0H31.5C32.8807 0 34 1.11929 34 2.5C34 3.88071 32.8807 5 31.5 5H2.5C1.11929 5 0 3.88071 0 2.5Z"
+				fill="#D8E6EB"
+			/>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M0 13.5C0 12.1193 1.11929 11 2.5 11H31.5C32.8807 11 34 12.1193 34 13.5C34 14.8807 32.8807 16 31.5 16H2.5C1.11929 16 0 14.8807 0 13.5Z"
+				fill="#D8E6EB"
+			/>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M0 24.5C0 23.1193 1.11929 22 2.5 22H31.5C32.8807 22 34 23.1193 34 24.5C34 25.8807 32.8807 27 31.5 27H2.5C1.11929 27 0 25.8807 0 24.5Z"
+				fill="#D8E6EB"
+			/>
+		</svg>
+	);
+}
