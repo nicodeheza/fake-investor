809bdb7e2460e7459a0ccbf4151efa7dd633e777
nicodeheza <nicolasdeheza@hotmail.com>
2022-03-05 16:31:30 -0300

    client login and sing up sketch

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -1,7 +1,45 @@
+import ilus1 from "../assets/ilus1.svg";
+import "./login-singup.css";
+
 export default function LogIn() {
 	return (
-		<div>
-			<h1>Log In</h1>
+		<div className="login">
+			<h1>
+				Start <span>training!</span>
+			</h1>
+			<main>
+				<div className="flash-ilus">
+					<div className="flash" />
+					<img src={ilus1} alt="illustration" />
+				</div>
+				<div className="login-card">
+					<form className="form-card">
+						<div className="form-fild-group">
+							<label htmlFor="email-user">Email or User Name</label>
+							<input
+								className="text-input"
+								type="text"
+								id="email-user"
+								name="email-user"
+								required
+							/>
+						</div>
+						<div className="form-fild-group">
+							<label htmlFor="email-password">Password</label>
+							<input
+								className="text-input"
+								type="password"
+								id="email-password"
+								name="email-password"
+								required
+							/>
+						</div>
+						<div className="form-btn-container">
+							<button className="form-btn">Log in</button>
+						</div>
+					</form>
+				</div>
+			</main>
 		</div>
 	);
 }
--- client/src/pages/SingUp.tsx
+++ client/src/pages/SingUp.tsx
@@ -1,7 +1,65 @@
+import ilus2 from "../assets/ilus2.svg";
+import "./login-singup.css";
+
 export default function SingUp() {
 	return (
-		<div>
-			<h1>Sing Up</h1>
+		<div className="login">
+			<h1>
+				Sing up to <span>start training!</span>
+			</h1>
+			<main>
+				<div className="flash-ilus">
+					<div className="flash" />
+					<img src={ilus2} alt="illustration" />
+				</div>
+				<div className="login-card">
+					<form className="form-card">
+						<div className="form-fild-group">
+							<label htmlFor="email">Email</label>
+							<input
+								className="text-input"
+								type="text"
+								id="email"
+								name="email"
+								required
+							/>
+						</div>
+						<div className="form-fild-group">
+							<label htmlFor="username">User Name</label>
+							<input
+								className="text-input"
+								type="text"
+								id="username"
+								name="username"
+								required
+							/>
+						</div>
+						<div className="form-fild-group">
+							<label htmlFor="password">Password</label>
+							<input
+								className="text-input"
+								type="password"
+								id="password"
+								name="password"
+								required
+							/>
+						</div>
+						<div className="form-fild-group">
+							<label htmlFor="repeatPassword">Repeat Password</label>
+							<input
+								className="text-input"
+								type="password"
+								id="repeatPassword"
+								name="repeatPassword"
+								required
+							/>
+						</div>
+						<div className="form-btn-container">
+							<button className="form-btn">Sing Up</button>
+						</div>
+					</form>
+				</div>
+			</main>
 		</div>
 	);
 }
