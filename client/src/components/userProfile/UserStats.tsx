52dbfeb708f3345320ee9a5a8ce1edcb09b5ce0f
nicodeheza <nicolasdeheza@hotmail.com>
2022-04-11 18:11:05 -0300

    refactor portfolio componet and UserStats

--- client/src/components/userProfile/UserStats.tsx
+++ client/src/components/userProfile/UserStats.tsx
@@ -1,14 +1,9 @@
-import {useState, useEffect} from "react";
-import {useNavigate} from "react-router-dom";
-import {API_URL} from "../../consts";
-import {UseUserName} from "../../context/UserContext";
-import Btn from "../Btn";
 import Arrow from "../../svg/Arrow";
 import roundTow from "../../helpers/roundTow";
 
 import "./userStats.css";
 
-interface stat {
+export interface stat {
 	gainMony: number;
 	gainPer: number;
 	liquidMon: number;
@@ -18,71 +13,44 @@ interface stat {
 	total: number;
 }
 
-export default function UserStats() {
-	const [stats, setStats] = useState<stat>();
-	const navigate = useNavigate();
-	const {setUserName} = UseUserName();
-
-	useEffect(() => {
-		fetch(`${API_URL}/user/userStats`, {
-			method: "GET",
-			credentials: "include"
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				if (data.userName === "") {
-					setUserName("");
-					navigate("/");
-				}
-				setStats(data);
-			})
-			.catch((err) => console.log(err));
-	}, [navigate, setUserName]);
-
+export default function UserStats({
+	gainMony,
+	gainPer,
+	liquidMon,
+	liquidPer,
+	stocksMon,
+	stocksPer,
+	total
+}: stat) {
 	return (
-		<div className="portfolio-stats">
-			<h2>Your Stats</h2>
-			{stats ? (
-				<table>
-					<tbody>
-						<tr>
-							<td className="portfolio-stats-col1">Gains</td>
-							<td
-								className={`portfolio-stats-col2 ${stats.gainMony > 0 ? "green" : "red"}`}
-							>
-								<div className="portfolio-stats-arrow">
-									<Arrow pos={stats!.gainMony > 0 ? "up" : "down"} />
-								</div>
-								{roundTow(stats.gainPer)}%
-							</td>
-							<td className="portfolio-stats-col3">{roundTow(stats.gainMony)} FUD</td>
-						</tr>
-						<tr>
-							<td className="portfolio-stats-col1">Liquid</td>
-							<td className="portfolio-stats-col2">{roundTow(stats.liquidPer)}%</td>
-							<td className="portfolio-stats-col3">{roundTow(stats.liquidMon)} FUD</td>
-						</tr>
-						<tr>
-							<td className="portfolio-stats-col1">Stocks</td>
-							<td className="portfolio-stats-col2">{roundTow(stats.stocksPer)}%</td>
-							<td className="portfolio-stats-col3">{roundTow(stats.stocksMon)} FUD</td>
-						</tr>
-						<tr>
-							<td className="portfolio-stats-col1">Total</td>
-							<td className="portfolio-stats-col2">{roundTow(stats.total)} FUD</td>
-							<td></td>
-						</tr>
-					</tbody>
-				</table>
-			) : null}
-			<div className="portfolio-stats-btn-container">
-				<Btn
-					text="Stock Trading"
-					padding="10px 20px"
-					onClick={() => navigate("/search")}
-				/>
-			</div>
-		</div>
+		<table>
+			<tbody>
+				<tr>
+					<td className="portfolio-stats-col1">Gains</td>
+					<td className={`portfolio-stats-col2 ${gainMony > 0 ? "green" : "red"}`}>
+						<div className="portfolio-stats-arrow">
+							<Arrow pos={gainMony > 0 ? "up" : "down"} />
+						</div>
+						{roundTow(gainPer)}%
+					</td>
+					<td className="portfolio-stats-col3">{roundTow(gainMony)} FUD</td>
+				</tr>
+				<tr>
+					<td className="portfolio-stats-col1">Liquid</td>
+					<td className="portfolio-stats-col2">{roundTow(liquidPer)}%</td>
+					<td className="portfolio-stats-col3">{roundTow(liquidMon)} FUD</td>
+				</tr>
+				<tr>
+					<td className="portfolio-stats-col1">Stocks</td>
+					<td className="portfolio-stats-col2">{roundTow(stocksPer)}%</td>
+					<td className="portfolio-stats-col3">{roundTow(stocksMon)} FUD</td>
+				</tr>
+				<tr>
+					<td className="portfolio-stats-col1">Total</td>
+					<td className="portfolio-stats-col2">{roundTow(total)} FUD</td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
 	);
 }
--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -1,9 +1,32 @@
-import UserStats from "../components/userProfile/UserStats";
+import {useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+import UserStats, {stat} from "../components/userProfile/UserStats";
+import {API_URL} from "../consts";
 import {UseUserName} from "../context/UserContext";
+import Btn from "../components/Btn";
 import "./portfolio.css";
 
 export default function Portfolio() {
-	const {userName} = UseUserName();
+	const {userName, setUserName} = UseUserName();
+	const [stats, setStats] = useState<stat>();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		fetch(`${API_URL}/user/userStats`, {
+			method: "GET",
+			credentials: "include"
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				if (data.userName === "") {
+					setUserName("");
+					navigate("/");
+				}
+				setStats(data);
+			})
+			.catch((err) => console.log(err));
+	}, [navigate, setUserName]);
 
 	return (
 		<div className="portfolio">
@@ -15,7 +38,27 @@ export default function Portfolio() {
 				<img src="./assets/sparck.svg" alt="sparck" />
 			</header>
 			<div className="portfolio-top-container">
-				<UserStats />
+				<div className="portfolio-stats">
+					<h2>Your Stats</h2>
+					{stats ? (
+						<UserStats
+							gainMony={stats.gainMony}
+							gainPer={stats.gainPer}
+							liquidMon={stats.liquidMon}
+							liquidPer={stats.liquidPer}
+							stocksMon={stats.stocksMon}
+							stocksPer={stats.stocksPer}
+							total={stats.total}
+						/>
+					) : null}
+					<div className="portfolio-stats-btn-container">
+						<Btn
+							text="Stock Trading"
+							padding="10px 20px"
+							onClick={() => navigate("/search")}
+						/>
+					</div>
+				</div>
 				<div className="portfolio-chart"></div>
 			</div>
 		</div>
