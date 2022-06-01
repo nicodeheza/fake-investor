57ae01203ec0df426fd4cd1cfa8ac73dda67c74f
nicodeheza <nicolasdeheza@hotmail.com>
2022-06-01 17:57:55 -0300

    login controller instegration test

--- server/src/tests/routes/userRoutes/loginTest.ts
+++ server/src/tests/routes/userRoutes/loginTest.ts
@@ -6,8 +6,75 @@ chai.use(chaiHttp);
 
 describe("/user/login route", function () {
 	describe("login with valid user and password", function () {
-		it("status must be 200", function () {});
-		it("userName must be returned", function () {});
+		let res: ChaiHttp.Response, err: any;
+		before(function (done) {
+			chai
+				.request(app)
+				.post("/api/user/login")
+				.set("Content-Type", "application/json")
+				.send({
+					email: "testUser@test.com",
+					password: "test"
+				})
+				.end(function (e, r) {
+					res = r;
+					err = e;
+					done();
+				});
+		});
+		it("status must be 200", function () {
+			expect(res).to.have.status(200);
+		});
+		it("userName must be returned", function () {
+			expect(res.body.userName).to.be.equal("testUser");
+		});
 	});
-	describe("login with invalid user and password", function () {});
+	describe("login with invalid password", function () {
+		let res: ChaiHttp.Response, err: any;
+		before(function (done) {
+			chai
+				.request(app)
+				.post("/api/user/login")
+				.set("Content-Type", "application/json")
+				.send({
+					email: "testUser@test.com",
+					password: "badPassword"
+				})
+				.end(function (e, r) {
+					res = r;
+					err = e;
+					done();
+				});
+		});
+		it("status must be 401", function () {
+			expect(res).to.have.status(401);
+		});
+		it("must response an empty object", function () {
+			expect(res.body).to.be.eql({});
+		});
+	});
+	describe("login with invalid user", function () {
+		let res: ChaiHttp.Response, err: any;
+		before(function (done) {
+			chai
+				.request(app)
+				.post("/api/user/login")
+				.set("Content-Type", "application/json")
+				.send({
+					email: "badUser@test.com",
+					password: "test"
+				})
+				.end(function (e, r) {
+					res = r;
+					err = e;
+					done();
+				});
+		});
+		it("status must be 401", function () {
+			expect(res).to.have.status(401);
+		});
+		it("must response an empty object", function () {
+			expect(res.body).to.be.eql({});
+		});
+	});
 });
